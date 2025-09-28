907997fff889019ae0a4fb18afee8dd27d02cd2b
Nguyen Anh Duy <duy.nguyen280805@hcmut.edu.vn>
2025-09-28 23:33:18 +0700

    Ball Physic Materials and Player Interaction with Ball and TileMap Collider

--- kick-&-score/Scripts/player.gd
+++ kick-&-score/Scripts/player.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 
 const SPEED = 50.0
 var current_dir = "none"
+var pushing_ball = false
 
 func _ready() -> void:
 	$AnimatedSprite2D.play("idle")
 
 func _physics_process(delta: float) -> void:
 	player_movement(delta)
-	
-func player_movement(delta: float):
+	check_collision(delta)
+
+func player_movement(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
 		play_anim(1)
@@ -18,7 +20,7 @@ func player_movement(delta: float):
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
 		play_anim(1)
-		velocity.x = - SPEED
+		velocity.x = -SPEED
 		velocity.y = 0
 	elif Input.is_action_pressed("ui_down"):
 		current_dir = "down"
@@ -29,15 +31,16 @@ func player_movement(delta: float):
 		current_dir = "up"
 		play_anim(1)
 		velocity.x = 0
-		velocity.y = - SPEED
+		velocity.y = -SPEED
 	else:
 		play_anim(0)
-		velocity.x = 0 
-		velocity.y = 0
-		
-	move_and_slide()
+		velocity = Vector2.ZERO
 
-func play_anim(movement):
+	move_and_slide()
+	if pushing_ball:
+		push_ball()
+
+func play_anim(movement: int) -> void:
 	var dir = current_dir
 	var anim = $AnimatedSprite2D
 	
@@ -68,3 +71,22 @@ func play_anim(movement):
 			anim.play("walk_front")
 		elif movement == 0:
 			anim.play("idle")
+
+func push_ball() -> void:
+	var ball = get_node_or_null("Ball") # Use get_node_or_null to avoid errors if the node doesn't exist
+	if ball:
+		print("Ball node found!")
+	else:
+		print("Ball node not found!")
+	if ball and ball is RigidBody2D:
+		ball.apply_impulse(Vector2.ZERO, velocity.normalized() * 1000)
+
+func check_collision(delta: float) -> void:
+	var collision = move_and_collide(velocity * delta)
+	if collision:
+		var collider = collision.get_collider()
+		if collider.name == "Ball":
+			pushing_ball = true
+			print("Player is now in collision with the ball!")
+		else:
+			pushing_ball = false
