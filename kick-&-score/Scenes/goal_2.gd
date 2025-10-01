6bb0cc72d9a2ec6cc56340e2d37a4f1ad7146296
HatakekkSheeshh <nguyenquochieujff7@gmail.com>
2025-10-01 14:12:39 +0700

    Intro and UI

--- kick-&-score/Scenes/goal_2.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-func _ready():
-	connect("body_entered", Callable(self, "_on_Goal_body_entered"))
-
-func _on_Goal_body_entered(body):
-	if body.is_in_group("ball"):
-		print("The ball is in goal2!")
